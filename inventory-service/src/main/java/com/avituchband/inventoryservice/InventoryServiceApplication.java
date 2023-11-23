a9c7babeb255f3b3d401f37d5f57d5146ec25422
Avi Tuchband <avitoohband@gmail.com>
2023-11-23 15:40:41 +0200

    implemented Eureka, Api Gateway and LoadBalanced WebClient

--- inventory-service/src/main/java/com/avituchband/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/avituchband/inventoryservice/InventoryServiceApplication.java
@@ -5,32 +5,30 @@ import com.avituchband.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventoryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
-		return args -> {
-			Inventory inventory = new Inventory();
-			inventory.setSkuCode("Iphone_13");
-			inventory.setQuantity(100);
+    @Bean
+    public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+        return args -> {
+            Inventory inventory = new Inventory();
+            inventory.setSkuCode("Iphone_13");
+            inventory.setQuantity(100);
 
-			Inventory inventory2 = new Inventory();
-			inventory2.setSkuCode("Iphone_13_red");
-			inventory2.setQuantity(0);
+            Inventory inventory2 = new Inventory();
+            inventory2.setSkuCode("Iphone_13_red");
+            inventory2.setQuantity(0);
 
-			inventoryRepository.save(inventory);
-			inventoryRepository.save(inventory2);
-		};
-	}
+            inventoryRepository.save(inventory);
+            inventoryRepository.save(inventory2);
+        };
+    }
 
 }
