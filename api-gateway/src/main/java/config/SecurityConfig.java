11a9177c161df039b3e87f0ec211b68085c36e76
Avi Tuchband <avitoohband@gmail.com>
2023-11-25 21:29:06 +0200

    started SecurityConfig file in Api Gateway

---
+++ api-gateway/src/main/java/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+        //03:11:14
+}
