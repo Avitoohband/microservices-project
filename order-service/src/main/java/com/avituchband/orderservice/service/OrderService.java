9a7162b3079cddf6e6d78d26d89040eb4fcdeb1c
Avi Tuchband <Avitoohband@gmail.com>
2024-01-17 15:11:07 +0200

    notification + kafaka

--- order-service/src/main/java/com/avituchband/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/avituchband/orderservice/service/OrderService.java
@@ -3,10 +3,14 @@ package com.avituchband.orderservice.service;
 import com.avituchband.orderservice.dto.InventoryResponse;
 import com.avituchband.orderservice.dto.OrderLineItemsDto;
 import com.avituchband.orderservice.dto.OrderRequest;
+import com.avituchband.orderservice.event.OrderPlacedEvent;
 import com.avituchband.orderservice.model.Order;
 import com.avituchband.orderservice.model.OrderLineItems;
 import com.avituchband.orderservice.repository.OrderRepository;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -23,6 +27,8 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -37,6 +43,7 @@ public class OrderService {
 
         if (Boolean.TRUE.equals(getIsInStock(order))) {
             orderRepository.save(order);
+            kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
             return "Order placed Successfully!";
         } else {
             throw new IllegalArgumentException("Product is not in stock, please try again later.");
@@ -51,19 +58,27 @@ public class OrderService {
                 .map(orderLineItems -> "skuCodeList=" + orderLineItems.getSkuCode()).
                 collect(Collectors.joining("&"));
 
-        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
-                .uri("http://inventory-service/api/inventory?" + skuCodeParams)
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class)
-                .block();
+        Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
+        try (Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())) {
+            InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory?" + skuCodeParams)
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
 
-        // Call Inventory Service, and place order in product is in stock
-        if (inventoryResponseArray != null) {
-            return Arrays.stream(inventoryResponseArray)
-                    .allMatch(InventoryResponse::getIsInStock);
-        } else {
-            return false;
+            // Call Inventory Service, and place order in product is in stock
+            if (inventoryResponseArray != null) {
+                return Arrays.stream(inventoryResponseArray)
+                        .allMatch(InventoryResponse::getIsInStock);
+            } else {
+                return false;
+            }
+
+        } finally {
+            inventoryServiceLookup.end();
         }
+
+
     }
 
     public OrderLineItems toOrderLineItems(OrderLineItemsDto orderLineItemsDto) {
